806a7073af4a4aea9c0157d9e9bebdc527b34927
Gabriel Paiva <gabriel.paiva0@icloud.com>
2023-05-19 09:43:56 -0300

    chore: added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 
 import AppRoutes from './routes/routes'
+import { inject } from '@vercel/analytics'
 
 import './services/firebase'
 import './index.css'
+
+inject()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
